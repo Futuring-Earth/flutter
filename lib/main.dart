6b0b0452a94eaf3a66624e48ef26e340888e3fb3
Eric <epomeranz@gmail.com>
2020-04-25 06:43:58 +0200

    Added more themes and make the app only accept portrait mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:app/view_models/activity/activity_viewmodel.dart';
 import 'package:app/views/activity/activity_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitUp,
+  ]);
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -26,6 +33,23 @@ class MyApp extends StatelessWidget {
             primarySwatch: Colors.purple,
             accentColor: Colors.deepOrange,
             fontFamily: 'Lato',
+            textTheme: ThemeData.light().textTheme.copyWith(
+                  title: TextStyle(
+                    fontFamily: 'OpenSans',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                  button: TextStyle(color: Colors.white),
+                ),
+            appBarTheme: AppBarTheme(
+              textTheme: ThemeData.light().textTheme.copyWith(
+                    title: TextStyle(
+                      fontFamily: 'OpenSans',
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+            ),
           ),
           home: ActivityView(),
           routes: {
