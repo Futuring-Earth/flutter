9e1c5eb31ccbb76333edc5e9fb87a00f05ed3642
Eric <epomeranz@gmail.com>
2020-04-30 18:14:14 +0200

    Clean up some code

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
-import 'package:app/ui/views/activity/activity_view.dart';
-import 'package:app/ui/views/settings/settings_view.dart';
-import './ui/views/activity/activity_view.dart';
 import './ui/views/tabs_view.dart';
-import './ui/views/profile/profile_view.dart';
-import './ui/views/inspiration/inspiration_view.dart';
-import './ui/views/challenges/challenges_view.dart';
-import './ui/views/challenges/edit_challenge_view.dart';
 import './core/services/auth_service.dart';
 import './provider_setup.dart';
 import './ui/views/auth_view.dart';
 import './ui/views/splash_view.dart';
 import './locator.dart';
+import './ui/layout/futuring_settings.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -34,28 +28,7 @@ class MyApp extends StatelessWidget {
       child: Consumer<Auth>(
         builder: (ctx, auth, _) => MaterialApp(
           title: 'Futuring',
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
-            accentColor: Colors.grey,
-            fontFamily: 'Lato',
-            textTheme: ThemeData.light().textTheme.copyWith(
-                  headline6: TextStyle(
-                    fontFamily: 'OpenSans',
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                  ),
-                  button: TextStyle(color: Colors.white),
-                ),
-            appBarTheme: AppBarTheme(
-              textTheme: ThemeData.light().textTheme.copyWith(
-                    headline6: TextStyle(
-                      fontFamily: 'OpenSans',
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-            ),
-          ),
+          theme: FuturingSettings.globalTheme,
           home: auth.isAuth
               ? TabsScreen()
               : FutureBuilder(
@@ -66,14 +39,7 @@ class MyApp extends StatelessWidget {
                           ? SplashView()
                           : AuthView(),
                 ),
-          routes: {
-            ProfileView.routeName: (ctx) => ProfileView(),
-            ActivityView.routeName: (ctx) => ActivityView(),
-            InspirationView.routeName: (ctx) => InspirationView(),
-            ChallengesView.routeName: (ctx) => ChallengesView(),
-            SettingsView.routeName: (ctx) => SettingsView(),
-            EditChallengesView.routeName: (ctx) => EditChallengesView(),
-          },
+          routes: FuturingSettings.getGlobalRoutes(ctx),
         ),
       ),
     );
---
+++ lib/ui/layout/futuring_settings.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import 'package:app/ui/views/activity/activity_view.dart';
+import 'package:app/ui/views/settings/settings_view.dart';
+import 'package:app/ui/views/profile/profile_view.dart';
+import 'package:app/ui/views/inspiration/inspiration_view.dart';
+import 'package:app/ui/views/challenges/challenges_view.dart';
+import 'package:app/ui/views/challenges/edit_challenge_view.dart';
+
+class FuturingSettings {
+  static ThemeData _androidTheme = ThemeData(
+    primarySwatch: Colors.blue,
+    accentColor: Colors.grey,
+    fontFamily: 'Lato',
+    textTheme: ThemeData.light().textTheme.copyWith(
+          headline6: TextStyle(
+            fontFamily: 'OpenSans',
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+          ),
+          button: TextStyle(color: Colors.white),
+        ),
+    appBarTheme: AppBarTheme(
+      textTheme: ThemeData.light().textTheme.copyWith(
+            headline6: TextStyle(
+              fontFamily: 'OpenSans',
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+    ),
+  );
+
+  static ThemeData globalTheme = _androidTheme;
+
+  static Map<String, Widget Function(BuildContext)> getGlobalRoutes(ctx) {
+    return _getAndroidRoutes(ctx);
+  }
+
+  static Map<String, Widget Function(BuildContext)> _getAndroidRoutes(ctx) {
+    return {
+      ProfileView.routeName: (ctx) => ProfileView(),
+      ActivityView.routeName: (ctx) => ActivityView(),
+      InspirationView.routeName: (ctx) => InspirationView(),
+      ChallengesView.routeName: (ctx) => ChallengesView(),
+      SettingsView.routeName: (ctx) => SettingsView(),
+      EditChallengesView.routeName: (ctx) => EditChallengesView(),
+    };
+  }
+}
